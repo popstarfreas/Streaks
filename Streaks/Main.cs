4c92c44de7e40cd4cf8289b31c237a4decdab10c
popstarfreas <6698249+popstarfreas@users.noreply.github.com>
2023-06-09 12:08:21 +0100

    Add missing changes

--- Streaks/Main.cs
+++ Streaks/Main.cs
@@ -10,66 +10,66 @@ using Microsoft.Xna.Framework;
 
 namespace Streaks
 {
-	[ApiVersion(2, 1)]
-	public class Streaks : TerrariaPlugin
-	{
-		public static readonly List<Player> Players = new List<Player>();
-		public DateTime LastUpdate = DateTime.UtcNow;
-		public Timer OnSecondUpdate;
+    [ApiVersion(2, 1)]
+    public class Streaks : TerrariaPlugin
+    {
+        public static readonly List<Player> Players = new List<Player>();
+        public DateTime LastUpdate = DateTime.UtcNow;
+        public Timer OnSecondUpdate;
         public static Config Config = new Config();
         public static DateTime[] Times = new DateTime[255];
         private Random Rand = new Random();
 
         public override string Author
-		{
-			get
-			{
-				return "popstarfreas";
-			}
-		}
+        {
+            get
+            {
+                return "popstarfreas";
+            }
+        }
 
-		public override string Description
-		{
-			get
-			{
-				return "Adds Streaks";
-			}
-		}
+        public override string Description
+        {
+            get
+            {
+                return "Adds Streaks";
+            }
+        }
 
-		public override string Name
-		{
-			get
-			{
-				return "Streaks";
-			}
-		}
+        public override string Name
+        {
+            get
+            {
+                return "Streaks";
+            }
+        }
 
-		public override Version Version
-		{
-			get
-			{
-				return new Version(1, 2, 0);
-			}
-		}
+        public override Version Version
+        {
+            get
+            {
+                return new Version(1, 2, 0);
+            }
+        }
 
-		public Streaks(Main game) : base(game)
+        public Streaks(Main game) : base(game)
         {
             Order = 14;
         }
-		
-		public override void Initialize()
-		{
-			ServerApi.Hooks.NetGreetPlayer.Register(this, GreetPlayer);
-			ServerApi.Hooks.ServerLeave.Register(this, PlayerLeave);
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.NetGreetPlayer.Register(this, GreetPlayer);
+            ServerApi.Hooks.ServerLeave.Register(this, PlayerLeave);
             ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
 
             GetDataHandlers.InitGetDataHandler();
 
-			TShockAPI.Commands.ChatCommands.Add (new Command ("streak.check", CheckStreakCommand, "streak"));
+            TShockAPI.Commands.ChatCommands.Add(new Command("streak.check", CheckStreakCommand, "streak"));
             TShockAPI.Commands.ChatCommands.Add(new Command("streak.reload", Reload, "streaksreload"));
-            OnSecondUpdate = new Timer (1000);
-			OnSecondUpdate.Enabled = true;
-			OnSecondUpdate.Elapsed += SecondUpdate;
+            OnSecondUpdate = new Timer(1000);
+            OnSecondUpdate.Enabled = true;
+            OnSecondUpdate.Elapsed += SecondUpdate;
 
             string path = Path.Combine(TShock.SavePath, "Streaks.json");
             if (!File.Exists(path))
@@ -89,6 +89,13 @@ namespace Streaks
                 return;
             }
 
+            // Killstreaks are only tracked for players who are logged-in
+            // but deathstreaks are tracked for everyone
+            if (args.Killer.User == null)
+            {
+                return;
+            }
+
             var killer = Players.FirstOrDefault(p => p != null && p.index == args.Killer.Index);
             var victim = args.Killer.Index != args.Victim.Index ? Players.FirstOrDefault(p => p != null && p.index == args.Victim.Index) : null;
 
@@ -97,7 +104,7 @@ namespace Streaks
                 if (victim.Streak >= 5)
                 {
                     int es = Rand.Next(Config.EndStreakMessages.Count());
-                    TSPlayer.All.SendMessage(String.Format( Config.EndStreakMessages[es], killer.Name, victim.Name, victim.Streak), 170, 0, 255);
+                    TSPlayer.All.SendMessage(String.Format(Config.EndStreakMessages[es], killer.Name, victim.Name, victim.Streak), 170, 0, 255);
                     victim.Streak = -1;
                 }
                 else
@@ -127,31 +134,37 @@ namespace Streaks
         }
 
         private void SecondUpdate(object sender, ElapsedEventArgs args)
-		{
-			lock (Players) {
-				foreach (var player in Players.Where(p => p != null)) {
-					if (player != null) {
-						if (player.PendingStreakClear) {
-							player.PendingStreakClear = false;
-							player.Streak = 0;
-							player.TsPlayer.SendMessage ("You were out of PvP too long, so your Streak was reset!", Color.Yellow);
-						}
+        {
+            lock (Players)
+            {
+                foreach (var player in Players.Where(p => p != null))
+                {
+                    if (player != null)
+                    {
+                        if (player.PendingStreakClear)
+                        {
+                            player.PendingStreakClear = false;
+                            player.Streak = 0;
+                            player.TsPlayer.SendMessage("You were out of PvP too long, so your Streak was reset!", Color.Yellow);
+                        }
 
-						if (!player.TSPlayer.hostile && player.Streak >= 1) {
-							player.PendingStreakClear = true;
-						}
-					}
-				}
-			}
-		}
+                        if (!player.TSPlayer.hostile && player.Streak >= 1)
+                        {
+                            player.PendingStreakClear = true;
+                        }
+                    }
+                }
+            }
+        }
 
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing) {
-				ServerApi.Hooks.NetGreetPlayer.Deregister (this, GreetPlayer);
-				ServerApi.Hooks.ServerLeave.Deregister (this, PlayerLeave);
-                base.Dispose (disposing);
-			}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.NetGreetPlayer.Deregister(this, GreetPlayer);
+                ServerApi.Hooks.ServerLeave.Deregister(this, PlayerLeave);
+                base.Dispose(disposing);
+            }
         }
         void Reload(CommandArgs e)
         {
@@ -163,29 +176,30 @@ namespace Streaks
         }
 
         private void GreetPlayer(GreetPlayerEventArgs args)
-		{
+        {
             if (TShock.Players[args.Who] == null) return;
             var exists = Players.Where(p => p != null && p.index == args.Who).Count() > 0;
-			if (exists) {
-				Players.RemoveAll (p => p != null && p.index == args.Who);
-			}
-			Players.Add (new Player (args.Who));
-		}
+            if (exists)
+            {
+                Players.RemoveAll(p => p != null && p.index == args.Who);
+            }
+            Players.Add(new Player(args.Who));
+        }
 
-		private void PlayerLeave(LeaveEventArgs args)
-		{
-			Players.RemoveAll (p => p != null && p.index == args.Who);
-		}
+        private void PlayerLeave(LeaveEventArgs args)
+        {
+            Players.RemoveAll(p => p != null && p.index == args.Who);
+        }
 
-		private void CheckStreakCommand(CommandArgs args)
-		{
-			var player = Players.FirstOrDefault (p => p != null && p.index == args.Player.Index);
+        private void CheckStreakCommand(CommandArgs args)
+        {
+            var player = Players.FirstOrDefault(p => p != null && p.index == args.Player.Index);
 
-			if (player == null)
-				return;
+            if (player == null)
+                return;
 
-			player.TsPlayer.SendMessage (String.Format("You are on a {0} {1}streak.", Math.Abs(player.Streak), player.Streak > -1 ? "Kill" : "Death"), Color.Yellow);
-		}
-	}
+            player.TsPlayer.SendMessage(String.Format("You are on a {0} {1}streak.", Math.Abs(player.Streak), player.Streak > -1 ? "Kill" : "Death"), Color.Yellow);
+        }
+    }
 }
 
--- Streaks/Player.cs
+++ Streaks/Player.cs
@@ -3,19 +3,19 @@ using TShockAPI;
 
 namespace Streaks
 {
-	public class Player
-	{
-		public readonly int index;
-		public TSPlayer TsPlayer { get { return TShock.Players[index]; } }
-		public Terraria.Player TSPlayer { get { return TShock.Players[index].TPlayer; } }
-		public string Name { get { return TShock.Players[index].Name; } }
-		public int Streak = 0;
-		public bool PendingStreakClear = false;
+    public class Player
+    {
+        public readonly int index;
+        public TSPlayer TsPlayer { get { return TShock.Players[index]; } }
+        public Terraria.Player TSPlayer { get { return TShock.Players[index].TPlayer; } }
+        public string Name { get { return TShock.Players[index].Name; } }
+        public int Streak = 0;
+        public bool PendingStreakClear = false;
 
-		public Player (int index)
-		{
-			this.index = index;
-		}
-	}
+        public Player(int index)
+        {
+            this.index = index;
+        }
+    }
 }
 
